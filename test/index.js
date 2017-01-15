1d42b3f64e7f7740b1ca58ce011e5dd8eda47d89
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2017-01-15 22:01:21 +0100

    Fix 'Connection is closed' errors when cleaning up tests

--- test/index.js
+++ test/index.js
@@ -295,10 +295,15 @@ function init(options){
   };
 }
 
+function noop(){}
+
 function cleanup(done){
   namespace1.server.close();
   namespace2.server.close();
   namespace3.server.close();
+  namespace1.adapter.on('error', noop);
+  namespace2.adapter.on('error', noop);
+  namespace3.adapter.on('error', noop);
   namespace1.adapter.subClient.quit();
   namespace2.adapter.subClient.quit();
   namespace3.adapter.subClient.quit();
