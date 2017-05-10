bceab01700d9a88e7954ec322fe199503a1ee4c4
Roman <root@zaraza.lt>
2017-05-11 02:10:16 +0300

    [fix] Fix duplicate identifier declaration (#213)

--- index.js
+++ index.js
@@ -233,15 +233,13 @@ function adapter(uri, opts) {
         var socket = this.nsp.connected[request.sid];
         if (!socket) { return; }
 
-        function sendAck(){
+        socket.join(request.room, function(){
           var response = JSON.stringify({
             requestid: request.requestid
           });
 
           pub.publish(self.responseChannel, response);
-        }
-
-        socket.join(request.room, sendAck);
+        });
         break;
 
       case requestTypes.remoteLeave:
@@ -249,15 +247,13 @@ function adapter(uri, opts) {
         var socket = this.nsp.connected[request.sid];
         if (!socket) { return; }
 
-        function sendAck(){
+        socket.leave(request.room, function(){
           var response = JSON.stringify({
             requestid: request.requestid
           });
 
           pub.publish(self.responseChannel, response);
-        }
-
-        socket.leave(request.room, sendAck);
+        });
         break;
 
       case requestTypes.remoteDisconnect:
